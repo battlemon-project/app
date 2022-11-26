aae1d83327cd1a2d763587276933ca7bb43db699
anbork <anbork@example.com>
2022-11-27 01:03:55 +0300

    Update loading screen

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image'
+
 export default function Loader() {
   return (
-    <div style={{background: '#000000'}} className="vh-100 w-100 position-absolute top-0">
+    <div style={{background: '#000000'}} className="vh-100 w-100 position-absolute top-0 left-0 d-flex align-items-center justify-content-center">
+      <Image src="/babylon.svg" alt="Babylon.js" width={260} height={260} />
     </div>
   )
 }
