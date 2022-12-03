144cd6a82e491479650fd9896068c00e77d08e52
anbork <anbork@example.com>
2022-12-04 01:26:54 +0300

    Update scene

--- scenes/Models/Home.ts
+++ scenes/Models/Home.ts
@@ -4,7 +4,7 @@ export const Home = async (scene: Scene): Promise<void> => {
   await SceneLoader.ImportMeshAsync(
     "",
     "/glb/",
-    "MainMenu_Stripes_Export_lemonprise.glb",
+    "MainMenu_Stripes_Export.glb",
     scene
   );
 
