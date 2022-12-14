eac91bbcff1375c37d2a4137baf5b4f3cb2bd471
anbork <anbork@example.com>
2022-12-14 21:54:13 +0300

    Update lemons platform

--- scenes/Models/NewLemon.ts
+++ scenes/Models/NewLemon.ts
@@ -3,10 +3,10 @@ import type { SuiMoveObject } from "@mysten/sui.js";
 
 export const NewLemon = async (scene: Scene, lemons: SuiMoveObject[]): Promise<void> => {
   const containers: { [key: string]: AssetContainer } = {}
-  const lastLemon = lemons[0];
 
   if (lemons && lemons.length) {
     const models = {
+      lemon: 'BTLMN_Lemon_A.glb',
       weapon: 'BTLMN_Outfit_Weapon_A.glb', 
       cap: 'BTLMN_Outfit_Cap_A.glb', 
       cloth: 'BTLMN_Outfit_Cloth_A.glb', 
@@ -18,89 +18,78 @@ export const NewLemon = async (scene: Scene, lemons: SuiMoveObject[]): Promise<v
         "/glb/", link, scene
       )
     }
-  }
 
+    lemons.slice(0, 3).forEach((suiLemon, index) => {
+      const lemonPosition = scene.getNodeByName(`LemonPos_${index + 1}`) as TransformNode
+      let lemonContainer = containers.lemon.instantiateModelsToScene((name) => name.split('_primitive')[0], false, { doNotInstantiate: true })
+      const lemon = lemonContainer.rootNodes[0];
+      lemon.scaling = new Vector3(120,120,120);
+      lemon.position.y = lemon.position.y + 9;
+      lemon.parent = lemonPosition;
+      lemon.rotation = lemonPosition.rotation;
 
-  if (lastLemon) {
-    const lemonScene = await SceneLoader.ImportMeshAsync(
-      "",
-      "/glb/",
-      "BTLMN_Lemon_A.glb",
-      scene
-    );
+      const outfits = getOutfits(suiLemon)
 
-
-    const lemonPosition = scene.getNodeByName(`LemonPos_1`) as TransformNode
-    const lemon = lemonScene.meshes[0];
-    lemon.scaling = new Vector3(100,100,100);
-    lemon.position.y = lemon.position.y + 9;
-    lemon.parent = lemonPosition;
-    lemon.rotation = lemonPosition.rotation;
-
-    const traits: { flavour: string, name: string }[] = (lastLemon as SuiMoveObject).fields.traits.map((trait: Record<string, string>) => trait.fields)
-
-    const clothTrait = traits.find(trait => trait.name === 'cloth');
-    if (clothTrait?.flavour == 'Cloth_Poncho_CA01') {
-      clothTrait.flavour = 'Cloth_Ninja_Waistband_NA01'
-    }
-    
-    const faceTrait = traits.find(trait => trait.name === 'face');
-    if (faceTrait?.flavour == 'Face_Gas_Mask_MA01') {
-      faceTrait.flavour = 'Face_Visor_VR_VR01'
-    }
-
-    const outfits: { [key: string]: { trait: { flavour: string, name: string } | undefined, placeholder: string }} = {
-      weapon: {
-        trait: traits.find(trait => trait.name === 'weapon'),
-        placeholder: 'placeholder_weapon_r'
-      },
-      cap: {
-        trait: traits.find(trait => trait.name === 'cap'),
-        placeholder: 'placeholder_cap'
-      },
-      cloth: {
-        trait: clothTrait,
-        placeholder: 'placeholder_cloth'
-      },
-      back: {
-        trait: traits.find(trait => trait.name === 'back'),
-        placeholder: 'placeholder_back'
-      },
-      face: {
-        trait: faceTrait,
-        placeholder: 'placeholder_face'
-      } 
-    }
-
-    const placeholder_test = scene.getMeshByName('placeholder_test')
-    if (placeholder_test) placeholder_test.visibility = 0;
-
-    const torso = scene.getNodeByName('torso') as Mesh
-    if (torso) torso.scaling = new Vector3(0,0,0);
-
-    for (const [key, { trait, placeholder }] of Object.entries(outfits)) {
-      const outfitContainer = containers[key].instantiateModelsToScene((name) => name.split('_primitive')[0], false, { doNotInstantiate: true })
-      const outfit = outfitContainer.rootNodes[0];
-
-      const all = outfit.getChildMeshes();
-      all.forEach(one => {
-        if (!one.name.includes(trait?.flavour || '')) {
-          one.visibility = 0;
+      for (const [key, { trait, placeholder }] of Object.entries(outfits)) {
+        const outfitContainer = containers[key].instantiateModelsToScene((name) => name.split('_primitive')[0], false, { doNotInstantiate: true })
+        const outfit = outfitContainer.rootNodes[0];
+  
+        const all = outfit.getChildMeshes();
+        all.forEach(one => {
+          if (!one.name.includes(trait?.flavour || '')) {
+            one.visibility = 0;
+          }
+        })
+        const placeholderNode = lemon.getChildTransformNodes().find(mesh => mesh.name == placeholder)
+        if (placeholderNode) {
+          outfit.parent = placeholderNode;  
+        }    
+      }
+      
+      lemonContainer.animationGroups.forEach(animationGroup => {
+        if (['placeholder_weapon_idle_001', 'lemon_idle_001', 'placeholder_head_idle_001'].includes(animationGroup.name)) {
+          animationGroup.start(true, 1)
         }
-      })
-      const placeholderNode = scene.getMeshByName(placeholder) as TransformNode
-      outfit.parent = placeholderNode;      
-    }
-    
-    const placeholder_weapon_idle_001 = scene.getAnimationGroupByName("placeholder_weapon_idle_001") as AnimationGroup
-    placeholder_weapon_idle_001.start(true, 1)
+      });
+    });
+  }
+}
 
-    const lemon_idle_001 = scene.getAnimationGroupByName("lemon_idle_001") as AnimationGroup
-    lemon_idle_001.start(true, 1)
+function getOutfits(lemon: SuiMoveObject) {
+  const traits: { flavour: string, name: string }[] = lemon.fields.traits.map((trait: Record<string, string>) => trait.fields)
 
-    const placeholder_head_idle_001 = scene.getAnimationGroupByName("placeholder_head_idle_001") as AnimationGroup
-    placeholder_head_idle_001.start(true, 1)
-    
+  const clothTrait = traits.find(trait => trait.name === 'cloth');
+  if (clothTrait?.flavour == 'Cloth_Poncho_CA01') {
+    clothTrait.flavour = 'Cloth_Ninja_Waistband_NA01'
+  }
+  
+  const faceTrait = traits.find(trait => trait.name === 'face');
+  if (faceTrait?.flavour == 'Face_Gas_Mask_MA01') {
+    faceTrait.flavour = 'Face_Visor_VR_VR01'
   }
 
+  const outfits: { [key: string]: { trait: { flavour: string, name: string } | undefined, placeholder: string }} = {
+    weapon: {
+      trait: traits.find(trait => trait.name === 'weapon'),
+      placeholder: 'placeholder_weapon_r'
+    },
+    cap: {
+      trait: traits.find(trait => trait.name === 'cap'),
+      placeholder: 'placeholder_cap'
+    },
+    cloth: {
+      trait: clothTrait,
+      placeholder: 'placeholder_cloth'
+    },
+    back: {
+      trait: traits.find(trait => trait.name === 'back'),
+      placeholder: 'placeholder_back'
+    },
+    face: {
+      trait: faceTrait,
+      placeholder: 'placeholder_face'
+    } 
+  }
+
+  return outfits;
 }
