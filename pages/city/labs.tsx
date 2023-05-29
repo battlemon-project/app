8a84e0a2eafc12945d98ed735a347177d65a61f0
dmitry <dmitry.tomahawk@yandex.ru>
2023-05-30 01:17:54 +0500

    implement layout

--- pages/city/labs.tsx
+++ pages/city/labs.tsx
@@ -10,6 +10,8 @@ import { craftGems, getGems, mintGem } from '../../helpers/alchemy';
 import { useAlert } from 'react-alert';
 import { CssLoader } from '../../components/CssLoader';
 import { BabylonLoader } from '../../components/BabylonLoader';
+import styles from './../../styles/Labs.module.css';
+import classNames from 'classnames';
 
 interface INft {
   id: string;
@@ -39,11 +41,8 @@ const Labs = () => {
     [string | null, string | null]
   >([null, null]);
   const { config: configMint } = usePrepareContractWrite(mintGem(address));
-  const {
-    data: dataMintGem,
-    write: sendMintGem,
-    isError: errorMintGem,
-  } = useContractWrite(configMint);
+  const { data: dataMintGem, isError: errorMintGem } =
+    useContractWrite(configMint);
   const { config: configCraft } = usePrepareContractWrite(
     craftGems(selectedGems[0], selectedGems[1])
   );
@@ -111,21 +110,6 @@ const Labs = () => {
     setLoader(false);
   };
 
-  const handleMintGem = async () => {
-    if (process.env.NEXT_PUBLIC_PRODUCTION == 'true') {
-      return false;
-    }
-    setLoader(true);
-
-    try {
-      await sendMintGem?.();
-    } catch (e) {
-      const { message } = e as Error;
-      alert.show(message, { type: 'error' });
-      setLoader(false);
-    }
-  };
-
   const handleSelectGem = (id: string) => () => {
     if (selectedGems[0] == id) {
       selectedGems[0] = null;
@@ -162,125 +146,101 @@ const Labs = () => {
     <>
       {!address && <BabylonLoader isConnected={false} />}
       <div className={`container ${address ? '' : 'd-none'}`}>
-        <div
-          className="inventory-container d-flex opened"
-          style={{ top: '30vh', height: '30vw', left: '32%' }}
-        >
-          <div className="inventory" style={{ height: '36%' }}>
-            <div
-              className="inventory-left-buttons d-flex flex-column"
-              style={{ width: '150px', left: '-160px' }}
-            >
-              <div
-                className="d-flex py-1 mt-5 mb-2 d-flex justify-content-center me-3"
-                style={{
-                  border: '1px solid #fff',
-                  borderRadius: '12px',
-                  minHeight: '135px',
-                }}
-              >
-                {selectedGems[0] != null ? (
-                  <img
-                    src={`/resources/assets/gems/${
-                      userGems.find((g) => g.id == selectedGems[0])?.image
-                    }`}
-                    alt="gem1"
-                    className="img-fluid"
-                  />
-                ) : (
-                  <div style={{ fontSize: '75px' }} className="text-white">
-                    +
-                  </div>
-                )}
+        <div className={styles.container}>
+          <div className={styles.inner__container}>
+            <div className={styles.wrapper}>
+              <div className={styles.left} style={{ width: '150px' }}>
+                <div className={styles.left__item}>
+                  {selectedGems[0] != null ? (
+                    <img
+                      src={`/resources/assets/gems/${
+                        userGems.find((g) => g.id == selectedGems[0])?.image
+                      }`}
+                      alt="gem1"
+                      className={styles.left__img}
+                    />
+                  ) : (
+                    <div className={styles.left__icon}>+</div>
+                  )}
+                </div>
+                <div className={styles.left__item}>
+                  {selectedGems[1] != null ? (
+                    <img
+                      src={`/resources/assets/gems/${
+                        userGems.find((u) => u.id == selectedGems[1])?.image
+                      }`}
+                      alt="gem1"
+                      className={styles.left__img}
+                    />
+                  ) : (
+                    <div className={styles.left__icon}>+</div>
+                  )}
+                </div>
               </div>
-              <div
-                className="d-flex py-1 mt-2 mb-4 d-flex justify-content-center me-3"
-                style={{
-                  border: '1px solid #fff',
-                  borderRadius: '12px',
-                  minHeight: '135px',
-                }}
-              >
-                {selectedGems[1] != null ? (
-                  <img
-                    src={`/resources/assets/gems/${
-                      userGems.find((u) => u.id == selectedGems[1])?.image
-                    }`}
-                    alt="gem1"
-                    className="img-fluid"
-                  />
-                ) : (
-                  <div style={{ fontSize: '75px' }} className="text-white">
-                    +
+              <div className={styles.inner}>
+                <div className={styles.tabs}>
+                  <div className={styles.tab}>Info</div>
+                  <div className={classNames(styles.tab, styles.active)}>
+                    Items
                   </div>
-                )}
+                </div>
+                <div className={styles.content}>
+                  {loader ? (
+                    <CssLoader />
+                  ) : userGems.length === 0 ? (
+                    <div
+                      className="col text-center"
+                      style={{ fontSize: '19px' }}
+                    >
+                      You have not gems yet
+                      {/* Click to PLUS for add sticker to Crafting */}
+                    </div>
+                  ) : (
+                    <div className={styles.list}>
+                      {userGems.map((gem) => (
+                        <div
+                          className={classNames(
+                            { [styles.active]: selectedGems.includes(gem.id) },
+                            styles.card
+                          )}
+                          key={gem.id}
+                        >
+                          <div
+                            className="link text-center py-2"
+                            onClick={handleSelectGem(gem.id)}
+                            style={{ cursor: 'pointer' }}
+                          >
+                            <img
+                              src={`/resources/assets/gems/${gem.image}`}
+                              alt={gem.id}
+                              className={styles.card__img}
+                            />
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-            <div className="inventory-scroll d-flex" style={{ width: '40vw' }}>
-              <div
-                className={`row w-100 d-flex ${
-                  !loader && userGems.length > 0
-                    ? 'align-self-start'
-                    : 'align-self-center'
-                }`}
-              >
-                {loader ? (
-                  <CssLoader />
-                ) : userGems.length === 0 ? (
-                  <div className="col text-center" style={{ fontSize: '19px' }}>
-                    You have not gems yet
-                    {/* Click to PLUS for add sticker to Crafting */}
-                  </div>
-                ) : (
-                  <>
-                    {userGems.map((gem) => (
-                      <div
-                        className={`col col-3 col-xl-2 border px-1 px-1 ${
-                          selectedGems.includes(gem.id) && 'selected'
-                        }`}
-                        key={gem.id}
-                      >
-                        <div
-                          className="link text-center py-2"
-                          onClick={handleSelectGem(gem.id)}
-                          style={{ cursor: 'pointer' }}
-                        >
-                          <img
-                            src={`/resources/assets/gems/${gem.image}`}
-                            alt={gem.id}
-                            className="img-fluid"
-                          />
-                        </div>
-                      </div>
-                    ))}
-                  </>
+            <div>
+              <button
+                className={classNames(
+                  {
+                    [styles.active]:
+                      !selectedGems.includes(null) &&
+                      userGems.find((g) => g.id == selectedGems[0])?.image ==
+                        userGems.find((g) => g.id == selectedGems[1])?.image,
+                  },
+                  styles.craft
                 )}
-              </div>
+                onClick={handleCraft}
+              >
+                <CraftIcon />
+                Craft
+              </button>
             </div>
           </div>
-        </div>
-
-        <div className="text-center position-relative" style={{ top: '66vh' }}>
-          <button
-            className="btn btn-secondary btn-lg px-3 pb-2 pt-2 me-3"
-            style={{ fontSize: '19px', lineHeight: '36px', opacity: 0.9 }}
-            onClick={handleMintGem}
-          >
-            Get Gems (Testnet)
-          </button>
-          <button
-            className={`btn btn-lg px-5 pb-3 pt-3 ${
-              !selectedGems.includes(null) &&
-              userGems.find((g) => g.id == selectedGems[0])?.image ==
-                userGems.find((g) => g.id == selectedGems[1])?.image
-                ? 'btn-primary'
-                : 'disabled btn-dark'
-            }`}
-            style={{ fontSize: '27px', lineHeight: '36px' }}
-            onClick={handleCraft}
-          >
-            Craft
-          </button>
         </div>
       </div>
 
@@ -312,6 +272,26 @@ const Labs = () => {
         </div>
       </div>
     </>
+  );
+};
+
+const CraftIcon: React.FC = () => {
+  return (
+    <svg
+      width="23"
+      height="22"
+      viewBox="0 0 23 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 21V16M4 6V1M1.5 3.5H6.5M1.5 18.5H6.5M12.5 2L10.7658 6.50886C10.4838 7.24209 10.3428 7.60871 10.1235 7.91709C9.92919 8.1904 9.6904 8.42919 9.41709 8.62353C9.10871 8.84281 8.74209 8.98381 8.00886 9.26582L3.5 11L8.00886 12.7342C8.74209 13.0162 9.10871 13.1572 9.41709 13.3765C9.6904 13.5708 9.92919 13.8096 10.1235 14.0829C10.3428 14.3913 10.4838 14.7579 10.7658 15.4911L12.5 20L14.2342 15.4911C14.5162 14.7579 14.6572 14.3913 14.8765 14.0829C15.0708 13.8096 15.3096 13.5708 15.5829 13.3765C15.8913 13.1572 16.2579 13.0162 16.9911 12.7342L21.5 11L16.9911 9.26582C16.2579 8.98381 15.8913 8.8428 15.5829 8.62353C15.3096 8.42919 15.0708 8.1904 14.8765 7.91709C14.6572 7.60871 14.5162 7.24209 14.2342 6.50886L12.5 2Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   );
 };
 
